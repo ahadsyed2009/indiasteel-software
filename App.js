7f6ac072fcc9cd8fdbf09a34f220f2fbae0e9b61
ahadsyed2009 <ahadsyed2009@gmail.com>
2025-08-28 12:05:43 +0530

    Update App.js

--- App.js
+++ App.js
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import HomeScreen from './components/HomeScreen'
 import AddCustomer from './components/AddCustomer'
+import OrdersPage from './components/Order'
 
 
 const Drawer = createDrawerNavigator();
@@ -17,8 +18,10 @@ const Drawer = createDrawerNavigator();
       >
         <Drawer.Screen name="Home" component={HomeScreen} />
         <Drawer.Screen name="AddCustomer" component={AddCustomer} />
+      <Drawer.Screen name="OrdersPage" component={OrdersPage} />
+
+
       </Drawer.Navigator>
     </NavigationContainer>
   );
 }
-
